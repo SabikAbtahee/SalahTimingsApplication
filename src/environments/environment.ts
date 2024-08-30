66d5e02adbc411c7285f474c069e0b2eaf7e8f9f
Sabik Abtahee <sabik.abtahee@gmail.com>
2024-08-31 03:27:44 +0600

    chore: clear for build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,4 @@
 export const environment = {
-  PrayerTimings:
-    'https://us.mohid.co/tx/dallas/ialfm/masjid/widget/api/index/?m=prayertimings',
-  CurrentImageName: 'may1v2.png',
   SalahTimesService:
     'https://salahtimesservice-hqd9bdahckbnf8av.eastus-01.azurewebsites.net',
 };
